0c5efb7ea60229c43f7b759b76f46ca6ab286e9c
MrZICS <dzobaoksana1993@gmail.com>
2023-03-09 15:52:21 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,38 +1,9 @@
-// import React from 'react';
+ import React from 'react';
  import { ContactListStyled, ContactItem } from './ContactList.styled';
 
-// import { useDispatch, useSelector } from 'react-redux';
-// //import { deleteContact } from 'components/redux/contactSlice';
 
-// //import {  getFilteredContacts } from 'components/redux/selectors';
  import { Button } from 'components/App/App.styled';
-// import { deleteContacts } from '../redux/operations';
 
-
-// const ContactList = () => {
-//   // const contacts = useSelector(getFilteredContacts);
-
-//    const dispatch = useDispatch();
-   
-
-//   return (
-//     <ContactListStyled>
-//       {contacts.map(contact => (
-//         <ContactItem key={contact.id}>
-//           {contact.name}: {contact.number}
-//           <Button
-//             type="button"
-//             onClick={() => dispatch(deleteContacts(contact.id))}
-//           >
-//             Delete
-//           </Button>
-//         </ContactItem>
-//       ))}
-//     </ContactListStyled>
-//   );
-// };
-
-// export default ContactList;
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContacts } from '../redux/operations';
@@ -61,7 +32,7 @@ export const ContactsList = () => {
       {contacts.map(contact => (
         <ContactItem key={contact.id}>
           <ContactItem>
-            {contact.name}: {contact.phone}{' '}
+            {contact.name}: {contact.number}
             <Button onClick={() => dispatch(deleteContacts(contact.id))}>
               Delete
             </Button>
